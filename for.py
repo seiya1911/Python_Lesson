027a622d9b3de324e41819c6a96cb1060c1429bf
seiya1911 <n.siy1911cb@gmail.com>
2021-08-23 11:55:43 +0900

    [Add]for/while

---
+++ for.py
@@ -0,0 +1,7 @@
+mylist = ['Orange', 'Peach', 'Lemon', 'Apple']
+for val in mylist:
+    print('Value:' + val)
+
+mydict = {'L':'Lemon', 'O':'Orange', 'G':'Grapes'}
+for mykey, myvalue in mydict.items():
+    print('Key:' + mykey + ',value:' + myvalue)
---
+++ while.py
@@ -0,0 +1,8 @@
+num = 1
+print('Start')
+
+while num < 6:
+    print('num = ' + str(num))
+    num += 1
+
+print('END')
