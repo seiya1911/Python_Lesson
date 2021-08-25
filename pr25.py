7fdb1f46bebc3af9ba7f500e050784db8c38a8ee
seiya1911 <n.siy1911cb@gmail.com>
2021-08-25 12:59:50 +0900

    pr25.py

--- pr25.py
+++ pr25.py
@@ -10,7 +10,24 @@ def get_positive_numeral(x):
       if(x <= 0):
           return(x, 'は正の数値ではありません')
 
+def square_root(x):
+    '引数 x の平方根を求める'
+    rnew = x
+
+    diff = rnew - x/rnew
+    if(diff < 0):
+        diff = -diff
+    while(diff > 1.0E-6):
+        r1 = rnew
+        r2 = x/r1
+        rnew = (r1 + r2)/2
+        print(r1, rnew, r2)
+        diff = r1 - r2
+        if(diff < 0):
+            diff = -diff
+    return rnew
+
 v = input('正の数値を入力してください ')
 r = get_positive_numeral(v)
-print('結果は ', r)
-
+k = square_root(r)
+print('結果は ', k)
