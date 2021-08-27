d83ff898c48cee531735c864ba768453e38259a4
seiya1911 <n.siy1911cb@gmail.com>
2021-08-27 16:46:53 +0900

    [Change]tkdemo-2termToLambda

--- tkdemo-2term.py
+++ tkdemo-2term.py
@@ -22,44 +22,44 @@ def do_eq():
     current_number = 0
 
 
-def key1():
-    key(1)
+# def key1():
+#     key(1)
 
 
-def key2():
-    key(2)
+# def key2():
+#     key(2)
 
 
-def key3():
-    key(3)
+# def key3():
+#     key(3)
 
 
-def key4():
-    key(4)
+# def key4():
+#     key(4)
 
 
-def key5():
-    key(5)
+# def key5():
+#     key(5)
 
 
-def key6():
-    key(6)
+# def key6():
+#     key(6)
 
 
-def key7():
-    key(7)
+# def key7():
+#     key(7)
 
 
-def key8():
-    key(8)
+# def key8():
+#     key(8)
 
 
-def key9():
-    key(9)
+# def key9():
+#     key(9)
 
 
-def key0():
-    key(0)
+# def key0():
+#     key(0)
 
 
 def key(n):
@@ -93,16 +93,16 @@ root = tk.Tk()
 f = tk.Frame(root)
 f.grid()
 
-b1 = tk.Button(f, text='1', command=key1)
-b2 = tk.Button(f, text='2', command=key2)
-b3 = tk.Button(f, text='3', command=key3)
-b4 = tk.Button(f, text='4', command=key4)
-b5 = tk.Button(f, text='5', command=key5)
-b6 = tk.Button(f, text='6', command=key6)
-b7 = tk.Button(f, text='7', command=key7)
-b8 = tk.Button(f, text='8', command=key8)
-b9 = tk.Button(f, text='9', command=key9)
-b0 = tk.Button(f, text='0', command=key0)
+b1 = tk.Button(f, text='1', command=lambda: key(1))
+b2 = tk.Button(f, text='2', command=lambda: key(2))
+b3 = tk.Button(f, text='3', command=lambda: key(3))
+b4 = tk.Button(f, text='4', command=lambda: key(4))
+b5 = tk.Button(f, text='5', command=lambda: key(5))
+b6 = tk.Button(f, text='6', command=lambda: key(6))
+b7 = tk.Button(f, text='7', command=lambda: key(7))
+b8 = tk.Button(f, text='8', command=lambda: key(8))
+b9 = tk.Button(f, text='9', command=lambda: key(9))
+b0 = tk.Button(f, text='0', command=lambda: key(0))
 bc = tk.Button(f, text='C', command=clear)
 bp = tk.Button(f, text='+', command=plus)
 be = tk.Button(f, text='=', command=eq)
