785dc70d6c49cbd72782705b116842120a0c338f
seiya1911 <n.siy1911cb@gmail.com>
2021-08-29 19:18:17 +0900

    pg25.py

---
+++ pg25.py
@@ -0,0 +1,28 @@
+class MyClass():
+    a = 'マイクラス'
+    __b = 0
+
+    def __int__(self, data):
+        self.__number = MyClass.__b
+        self.mydata = data
+        print('MyClass Object is created, number: ', self.__number)
+        MyClass.__b += 1
+
+    def show_number(self):
+        print(self.__number)
+
+
+if __name__ == '__main__':
+    print('MyClass の変数 a:', MyClass.a)
+
+    instance1 = MyClass(1)
+    instance2 = MyClass(10)
+    instance1.show_number()
+    instance2.show_number()
+
+    print('mydata of instance1:', instance1.mydata)
+    print('mydata of instance2:', instance2.mydata)
+    instance1.mydata += 1
+    instance2.mydata += 2
+    print('mydata of instance1:', instance1.mydata)
+    print('mydata of instance2:', instance2.mydata)
