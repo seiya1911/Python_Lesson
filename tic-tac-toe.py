ca62ea811bc696d73e3c608aac6bad642e43e8d0
seiya1911 <n.siy1911cb@gmail.com>
2021-09-04 11:40:59 +0900

    tic-tac-toe.py-2

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,3 +13,38 @@ log2 = [[0, 0], [1, 0], [1, 1], [2, 2], [0, 1],
         [2, 0], [FIRST]]
 log3 = [[0, 1], [0, 0], [2, 1], [1, 1], [2, 2],
         [2, 0], [1, 0], [0, 2], [SECOND]]
+
+
+def show_turn():
+    '手番を示す文字列を返す'
+    if turn == FIRST:
+        return('先手')
+    elif turn == SECOND:
+        return('後手')
+    else:
+        return('手番の値が不適切です')
+
+
+def init_turn():
+    '手番を初期化する'
+    global turn
+    turn = 1
+
+
+def change_turn():
+    '手番を交代する'
+    global turn
+    if turn == FIRST:
+        turn = SECOND
+    elif turn == SECOND:
+        turn = FIRST
+
+
+def test_turn():
+    '手番をテストする'
+    init_turn()
+    print(show_turn(), 'の番です')
+    change_turn()
+    print(show_turn(), 'の番です')
+    change_turn()
+    print(show_turn(), 'の番です')
