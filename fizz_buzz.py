56fb8ba0a5bc7325635715ac091d76c4287815d4
seiya1911 <n.siy1911cb@gmail.com>
2021-09-14 15:35:24 +0900

    fizz_buzzToinput

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,21 @@
-i = 1
-for i in range(1, 7):
-    if i % 15 == 0:
-        print('Fizz Buzz!')
-    elif i % 3 == 0:
-        print('Fizz!')
-    elif i % 5 == 0:
-        print('Buzz!')
-    else:
-        print(i)
-    i += 1
+while True:
+    x = input('正の数値を入力してください ')
+    try:
+        x = float(x)
+        if x % 15 == 0:
+            print('Fizz Buzz!')
+        elif x % 3 == 0:
+            print('Fizz!')
+        elif x % 5 == 0:
+            print('Buzz!')
+        else:
+            print(x)
+    except ValueError:
+        print(x, 'は数値に変換できません')
+        continue
+    except:
+        print('予期していないエラーです')
+        exit()
+    if(x <= 0):
+        print(x, 'は正の数値ではありません')
+        continue
