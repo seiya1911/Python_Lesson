42948d03977b7c5366400e4529f36b0533dab49e
seiya1911 <n.siy1911cb@gmail.com>
2021-09-28 18:56:48 +0900

    use_read_csv.py

---
+++ use_read_csv.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+
+os.chdir(r"/Users/seiyanishikawa/Documents/python_scripts")
+df = pd.read_csv('sample2.csv')
+
+df['Total'] = df.sum(axis = 1)
+print(df)
+
+print(df,describe())
